a6f5197922abc4c0d76573f0a45ee41021e6f204
wuyeelok <wuyeelok@gmail.com>
2024-05-03 23:17:40 +0800

    Rendering HTML Pages without Spring
    https://www.marcobehler.com/courses/spring-professional/dependency-injection-basics/dependency-injection-rendering-html

---
+++ src/main/java/com/ken/MyFirstServlet.java
@@ -0,0 +1,23 @@
+package com.ken;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyFirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.getWriter().print(
+                """
+                        <html>
+                        <body>
+                        <h1>Hello World</h1>
+                        <p>This is my very first, embedded Tomcat, HTML Page!</p>
+                        </body>
+                        </html>""");
+    }
+}
